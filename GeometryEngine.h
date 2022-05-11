7e5cba0f0e9cc2bda72014d0161bb144fbdf4036
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2022-05-11 12:03:53 +0700

    Create GeometryEngine.h

---
+++ GeometryEngine.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QOpenGLBuffer>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QVector3D>
+#include "Window.h"
+
+struct VertexData {
+  QVector3D position;
+  QVector3D normal;
+  QVector3D colour;
+};
+
+class GeometryEngine {
+public:
+  GeometryEngine();
+
+  void drawCubeGeometry(QOpenGLShaderProgram *program,
+                        QOpenGLFunctions *functions);
+  void initCubeGeometry(float width, unsigned int factor);
+  void rotate(const QQuaternion &r);
+  void translate(const QVector3D &t);
+
+private:
+  QOpenGLBuffer arrayBuf;
+  QOpenGLBuffer indexBuf;
+
+  QQuaternion m_rotate;
+  QVector3D m_translate;
+};
